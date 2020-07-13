6fe022cbff6aac437717e0ebacd1b5726ad4fae4
Selim Ellieh <selimplayselim@gmail.com>
2020-07-14 00:17:28 +0300

    added jobs to homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    created = models.DateTimeField()
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="images/")
+    body = models.TextField()
