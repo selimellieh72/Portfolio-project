72f8d03c051c6ec6c617723b948b3c3ca9617a0f
Selim Ellieh <selimplayselim@gmail.com>
2020-07-22 14:48:04 +0300

    Added og tags

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import blog
 def allblogs(request):
     blogs = blog.objects
-    return render(request, "blog/allblogs.html", {"blog":blogs})
+    return render(request, "blog/allblogs.html", {"blogs":blogs})
 def detailedblog(request,blog_id):
     wholeblog = get_object_or_404(blog, pk=blog_id)
     return render(request, "blog/detailedblog.html", {'blog' : wholeblog})
